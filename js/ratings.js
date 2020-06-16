a178eba828d7790839f585366bd3e55e1b74f88b
Cory Henson <cogan17@gmail.com>
2020-06-16 16:28:08 -0700

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,4 +1,6 @@
 function collect_ratings() {
     var ratings = { count = "0", sum = "0", average = "0" };
-    let rating = "0"
+    let rating = "0";
 }
+
+const elements = document.querySelectorAll("rating")
